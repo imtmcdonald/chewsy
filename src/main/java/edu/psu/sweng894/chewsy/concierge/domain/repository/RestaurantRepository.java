8641d2780f7294b774ebd5ed5e31281ea715d412
tim <timmc9104@gmail.com>
2022-05-29 12:29:09 -0400

    AB#22 - Develop a client to pull a list of restaurants from the Google Places API

--- src/main/java/edu/psu/sweng894/chewsy/concierge/domain/repository/RestaurantRepository.java
+++ src/main/java/edu/psu/sweng894/chewsy/concierge/domain/repository/RestaurantRepository.java
@@ -3,5 +3,5 @@ package edu.psu.sweng894.chewsy.concierge.domain.repository;
 import org.json.simple.JSONArray;
 
 public interface RestaurantRepository {
-    public JSONArray findRestaurants(String location, int radius, String apiKey);
+    public JSONArray findRestaurants(String location, int radius);
 }
