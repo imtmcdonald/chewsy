c8fded6bcacdc3480f5ca4dc2d89087f4b8ecf38
tim <timmc9104@gmail.com>
2022-05-29 19:39:44 -0400

    AB#35 - Develop unit tests for the concierge service

--- src/test/java/edu/psu/sweng894/chewsy/concierge/infrastructure/repository/GooglePlacesAPI/GooglePlacesAPIRepositoryTest.java
+++ src/test/java/edu/psu/sweng894/chewsy/concierge/infrastructure/repository/GooglePlacesAPI/GooglePlacesAPIRepositoryTest.java
@@ -4,15 +4,16 @@ import edu.psu.sweng894.chewsy.concierge.domain.repository.RestaurantRepository;
 
 import org.junit.Test;
 
-import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import static org.junit.Assert.*; 
 
 public class GooglePlacesAPIRepositoryTest {
+
     @Test
-    public void testFindRestaurants() {
+    public void testFindRestaurantsNotNull() {
         RestaurantRepository restaurants = new GooglePlacesAPIRepository();
-        JSONArray restaurantList = restaurants.findRestaurants("23666", 5);
-        assertNull(restaurantList);
+        JSONObject restaurantList = restaurants.findRestaurants("23666", 5);
+        assertNotNull(restaurantList);
     }
 }
